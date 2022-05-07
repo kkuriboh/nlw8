54c0c009475612a87d541856e14065c0a8b3a8dc
Augusto Pieper <augustoopieper@gmail.com>
2022-05-07 01:42:19 -0300

    stage 5 add PORT variable

--- server/src/server.ts
+++ server/src/server.ts
@@ -10,6 +10,6 @@ app.use(cors())
 app.use(express.json())
 app.use(routes)
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
 	console.log('Server is running on port 3000')
 })
